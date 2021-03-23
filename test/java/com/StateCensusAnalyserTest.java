4d8e447664629b39703df8341bb1a1e0846e291e
seema thorat <seemagade97@gmail.com>
2021-03-23 13:50:11 +0530

    State File Incorrect Return Custom Exception Done

--- test/java/com/StateCensusAnalyserTest.java
+++ test/java/com/StateCensusAnalyserTest.java
@@ -7,7 +7,8 @@ import java.io.IOException;
 public class StateCensusAnalyserTest {
 
     public static final String STATE_CENSUS_DATA_CSV = "C:\\Users\\seema thorat\\IdeaProjects\\MyStateCensusAnalyser\\src\\main\\resources\\IndiaStateCensusData.csv";
-
+    public static final String STATE_CODE_CSV = "C:\\Users\\seema thorat\\IdeaProjects\\MyStateCensusAnalyser\\src\\main\\resources\\StateCode.CSV";
+    public static final String WRONG_FILE = "/seema.txt";
 
     @Test
     public void GivenTheStateCodesCsvFile_IfHasCorrectNumberOfRecords_ShouldReturnTrue() throws IOException {
@@ -17,6 +18,16 @@ public class StateCensusAnalyserTest {
             Assertions.assertEquals(29, count);
         } catch (CensusAnalyserException e) {
             e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void GivenTheStateCensusCsvFile_IfDoesntExist_ShouldThrowCensusAnalyserException() throws IOException {
+        try {
+            int count = StateCensusAnalyser.openCsvBuilder(WRONG_FILE, StateCensus.class);
+        } catch (CensusAnalyserException e) {
+            e.printStackTrace();
+            Assertions.assertEquals(CensusAnalyserException.CensusExceptionType.NO_SUCH_FILE, e.type);
         }
     }
 }
